527c339c7d84a7c24254ac4cb79014ad98045d20
Cameron Ingham <camji555@gmail.com>
2019-04-18 21:10:33 -0700

    Fixed app launch issue.

--- OpenInTerminal/AppDelegate.swift
+++ OpenInTerminal/AppDelegate.swift
@@ -12,7 +12,7 @@ import OpenInTerminalCore
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    func applicationDidFinishLaunching(_ notification: Notification) {
         DistributedNotificationCenter.default().addObserver(self, selector: #selector(openTerminal), name: NSNotification.Name("openTerminal"), object: nil)
         DistributedNotificationCenter.default().addObserver(self, selector: #selector(openITerm), name: NSNotification.Name("openITerm"), object: nil)
         DistributedNotificationCenter.default().addObserver(self, selector: #selector(openHyper), name: NSNotification.Name("openHyper"), object: nil)
@@ -24,6 +24,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         DistributedNotificationCenter.default().removeObserver(self, name: NSNotification.Name("openTerminal"), object: nil)
         DistributedNotificationCenter.default().removeObserver(self, name:NSNotification.Name("openITerm"), object: nil)
         DistributedNotificationCenter.default().removeObserver(self, name: NSNotification.Name("openHyper"), object: nil)
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        TerminalManager.shared.openTerminal()
+        return true
     }
     
     @objc func openTerminal() {
