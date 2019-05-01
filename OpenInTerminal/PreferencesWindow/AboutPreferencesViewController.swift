6be6e888c65bfaf6a3226039a4b4fbd008b577ac
Jianing Wang <wjnmailg@gmail.com>
2019-05-01 23:58:28 +0800

    feat(Main): add About page in Preferences

---
+++ OpenInTerminal/PreferencesWindow/AboutPreferencesViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AboutPreferencesViewController.swift
+//  OpenInTerminal
+//
+//  Created by Jianing Wang on 2019/5/1.
+//  Copyright © 2019 Jianing Wang. All rights reserved.
+//
+
+import Cocoa
+
+class AboutPreferencesViewController: PreferencesViewController {
+
+    @IBOutlet weak var versionLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let versionObject = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+        versionLabel.stringValue = versionObject as? String ?? ""
+    }
+    
+    @IBAction func githubButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "https://github.com/Ji4n1ng/OpenInTerminal") else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @IBAction func contactButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "mailto:contact@jianing.wang?subject=OpenInTerminal%20Feedback") else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @IBAction func twitterButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "https://twitter.com/Ji4n1ng") else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @IBAction func paypalButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "https://paypal.me/ji4n1ng") else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @IBAction func alipayButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "https://github.com/Ji4n1ng/OpenInTerminal") else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @IBAction func camji55ButtonClicked(_ sender: NSButton) {
+        guard let url = URL(string: "https://github.com/Camji55") else { return }
+        NSWorkspace.shared.open(url)   
+    }
+}
+
+class LinkButton: NSButton {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func resetCursorRects() {
+        addCursorRect(self.bounds, cursor: .pointingHand)
+    }
+}
